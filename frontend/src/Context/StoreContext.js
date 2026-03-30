6818ae9fb9a69b3306d47b7d18066ad8480ef3a9
ASIF SHAH <125040394+Asif-code34@users.noreply.github.com>
2026-03-30 17:04:05 +0530

    Update StoreContext.js

--- frontend/src/Context/StoreContext.js
+++ frontend/src/Context/StoreContext.js
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:8080"
+    const url = "https://food-app-backend-asge.onrender.com"
    const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
